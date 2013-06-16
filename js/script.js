ca55cd3d9718263acb354f3fccbea138e19d6528
Rahul Raina <raina.anurag@gmail.com>
2013-06-16 16:26:51 +0530

    added personal site

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {	
+	
+	if ($('.skills').length) {
+	 	$(window).scroll(function() {
+	        if ($(window).scrollTop() + $(window).height() - 100 >= $('.skills').offset().top) {
+	            if(!$('.skills').attr('loaded')) {
+	                $('.skills').attr('loaded', true);
+					var list = document.querySelectorAll("dd > p");
+					for(var i = 0; i < list.length; ++i) { $(list[i]).animate({width: list[i].innerHTML}, "slow"); }
+	            }
+	        }
+	    });
+	}
+	  
+	$("a[href^='http']").attr("target","_blank");
+	$("a[href^='//']").attr("target","_blank");
+	$("a[href^='mailto:']").attr("target","_blank");	
+	
+	$('.top').click(function () {
+		$('body,html').animate({
+				scrollTop: 0
+				}, 800);
+		return false;
+	});
+
+});
